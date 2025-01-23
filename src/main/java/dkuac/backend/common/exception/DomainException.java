0afdaeda629daa0b17d5cd67e5ec7344d0cb6b48
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-24 00:46:30 +0900

    chore: 도메인 예외 클래스를 생성한다.

---
+++ src/main/java/dkuac/backend/common/exception/DomainException.java
@@ -0,0 +1,27 @@
+package dkuac.backend.common.exception;
+
+
+import dkuac.backend.common.exception.error.BaseErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class DomainException extends RuntimeException {
+
+    private HttpStatus httpStatus;
+
+    private String code;
+
+    public DomainException(String message) {
+        super(message);
+    }
+    public DomainException(HttpStatus httpStatus, String message) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+    public DomainException(HttpStatus httpStatus, BaseErrorCode<?> errorCode) {
+        super(errorCode.getMessage());
+        this.httpStatus = httpStatus;
+        this.code = errorCode.name();
+    }
+}
