fe9459df796b68453ea4f044a5575686cd3a83b6
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-25 15:36:55 +0900

    feat: Member 커스텀 에러 코드를 생성한다.

---
+++ src/main/java/dkuac/backend/member/exception/MemberErrorCode.java
@@ -0,0 +1,23 @@
+package dkuac.backend.member.exception;
+
+import dkuac.backend.common.exception.DomainException;
+import dkuac.backend.common.exception.error.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberErrorCode implements BaseErrorCode<DomainException> {
+
+    MEMBER_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 정보를 저장하는데 실패했습니다.");
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    @Override
+    public DomainException toException() {
+        return new DomainException(httpStatus, this);
+    }
+}
