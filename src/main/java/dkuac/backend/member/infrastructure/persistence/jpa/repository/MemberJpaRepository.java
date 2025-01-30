62727c71dfcc82dad9cdb094ef379bad7e9b1463
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-30 12:53:06 +0900

    feat: 학번 및 이메일로 유저를 찾을 수 있다.

--- src/main/java/dkuac/backend/member/infrastructure/persistence/jpa/repository/MemberJpaRepository.java
+++ src/main/java/dkuac/backend/member/infrastructure/persistence/jpa/repository/MemberJpaRepository.java
@@ -3,6 +3,9 @@ package dkuac.backend.member.infrastructure.persistence.jpa.repository;
 import dkuac.backend.member.infrastructure.persistence.jpa.entity.MemberJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberJpaRepository extends JpaRepository<MemberJpaEntity, Long> {
-    MemberJpaEntity findByStudentNumber(int studentNumber );
+    Optional<MemberJpaEntity> findByStudentNumber(int studentNumber);
+    Optional<MemberJpaEntity> findByEmail(String email);
 }
