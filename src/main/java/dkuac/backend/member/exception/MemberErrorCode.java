37ca2be631c754312ae6364e0e5d9bff31fa636e
HoyeongJeon <hoyeong0302@gmail.com>
2025-01-30 12:52:04 +0900

    feat: 중복 회원 가입 방지용 에러코드를 생성한다.

--- src/main/java/dkuac/backend/member/exception/MemberErrorCode.java
+++ src/main/java/dkuac/backend/member/exception/MemberErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MemberErrorCode implements BaseErrorCode<DomainException> {
 
-    MEMBER_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 정보를 저장하는데 실패했습니다.");
+    MEMBER_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "사용자 정보를 저장하는데 실패했습니다."),
+    MEMBER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 가입된 사용자입니다.");
 
     private final HttpStatus httpStatus;
 
