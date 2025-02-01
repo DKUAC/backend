d644f9ebb34808210e12297b2f73b6dcbbbef66b
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-01 15:50:20 +0900

    feat: JwtAuthenticationFilter를 구현하고, CustomUserDetailsService를 만든다.

---
+++ src/main/java/dkuac/backend/core/application/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package dkuac.backend.core.application;
+
+import dkuac.backend.member.domain.entity.Member;
+import dkuac.backend.member.domain.repository.MemberRepository;
+import dkuac.backend.member.exception.MemberErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByStudentName(username).orElseThrow(MemberErrorCode.MEMBER_NOT_EXISTS::toException);
+
+        return User.builder()
+                .username(member.getName())
+                .password(member.getPassword().getValue())
+                .roles("USER")
+                .build();
+    }
+
+    public UserDetails loadUserById(Long id) throws UsernameNotFoundException {
+        Member member = memberRepository.findById(id).orElseThrow(MemberErrorCode.MEMBER_NOT_EXISTS::toException);
+
+        return User.builder()
+                .username(member.getName())
+                .password(member.getPassword().getValue())
+                .roles("USER")
+                .build();
+    }
+}
