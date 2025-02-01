fd1fcb507c76470ac40d2de5116dc1d85a0ff0a1
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-01 15:51:08 +0900

    feat: LoginService를 구현한다.

--- src/main/java/dkuac/backend/auth/application/LoginService.java
+++ src/main/java/dkuac/backend/auth/application/LoginService.java
@@ -1,4 +1,28 @@
 package dkuac.backend.auth.application;
 
+import dkuac.backend.auth.presentation.dto.reqeust.LoginRequest;
+import dkuac.backend.auth.presentation.dto.response.LoginResponse;
+import dkuac.backend.member.domain.entity.Member;
+import dkuac.backend.member.domain.repository.MemberRepository;
+import dkuac.backend.member.exception.MemberErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class LoginService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordHashingService passwordHashingService;
+    private final TokenProvider tokenProvider;
+
+    public LoginResponse login(LoginRequest request) {
+        Member member = memberRepository.findByStudentNumber(request.studentNumber()).orElseThrow(MemberErrorCode.MEMBER_NOT_EXISTS::toException);
+        passwordHashingService.matches(request.password(), member.getPassword());
+        String accessToken = tokenProvider.provideAccessToken(member);
+        return LoginResponse.of(
+                member,
+                accessToken
+        );
+    }
 }
