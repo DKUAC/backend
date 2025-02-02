a0a317b398e6c3b8d34230f3842747f8863de45f
HoyeongJeon <hoyeong0302@gmail.com>
2025-02-02 16:04:14 +0900

    refactor: CustomUserDetails 를 구현한다.

--- src/main/java/dkuac/backend/core/application/CustomUserDetailsService.java
+++ src/main/java/dkuac/backend/core/application/CustomUserDetailsService.java
@@ -1,10 +1,10 @@
 package dkuac.backend.core.application;
 
+import dkuac.backend.core.infrastructure.security.CustomUserDetails;
 import dkuac.backend.member.domain.entity.Member;
 import dkuac.backend.member.domain.repository.MemberRepository;
 import dkuac.backend.member.exception.MemberErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,20 +19,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member member = memberRepository.findByStudentName(username).orElseThrow(MemberErrorCode.MEMBER_NOT_EXISTS::toException);
 
-        return User.builder()
-                .username(member.getName())
-                .password(member.getPassword().getValue())
-                .roles("USER")
-                .build();
+        return new CustomUserDetails(member);
     }
 
     public UserDetails loadUserById(Long id) throws UsernameNotFoundException {
         Member member = memberRepository.findById(id).orElseThrow(MemberErrorCode.MEMBER_NOT_EXISTS::toException);
 
-        return User.builder()
-                .username(member.getName())
-                .password(member.getPassword().getValue())
-                .roles("USER")
-                .build();
+        return new CustomUserDetails(member);
     }
 }
